a3f7061f6d5274fd25081dab06b1ec1e9a5a7235
AxelBuenoTome <145924762+AxelBuenoTome@users.noreply.github.com>
2023-10-22 20:17:45 +0200

    Update puzzle2.py
    Actualización sin Espera Activa

--- puzzle2.py
+++ puzzle2.py
@@ -2,8 +2,8 @@ import gi
 gi.require_version('Gtk', '3.0')
 from gi.repository import Gtk, GLib, Pango, Gdk
 
-from threading import Thread
-from puzzle1 import RfidReader  # Importa la clase desde el otro archivo
+from threading import Thread, Event
+from puzzle_1_4 import RfidReader  # Importa la clase desde el otro archivo
 
 class RFID_GUI(Gtk.Window):
     def __init__(self):
@@ -17,7 +17,7 @@ class RFID_GUI(Gtk.Window):
         self.label = Gtk.Label(label="Please, login with your university card", name="label")
         self.label.set_markup('<span size="x-large" weight= "bold" foreground="white">Please, login with your university card</span>')
         self.label.get_style_context().add_class("welcome-label")
-        self.label.override_background_color(Gtk.StateFlags.NORMAL, Gdk.RGBA(0, 0, 1, 1))  # Cambia el color de fondo a azul
+        self.label.override_background_color(Gtk.StateFlags.NORMAL, Gdk.RGBA(0.5, 0.5, 1, 1))  # Cambia el color de fondo a azul
         self.box.pack_start(self.label, True, True, 0)
 
         self.clear_button = Gtk.Button(label="Leer otro UID")
@@ -25,31 +25,34 @@ class RFID_GUI(Gtk.Window):
         self.clear_button.set_halign(Gtk.Align.CENTER)  # Centra horizontalmente
         self.clear_button.connect("clicked", self.clear_uid)
         self.box.pack_start(self.clear_button, False, False, 0) #Para que el botón no ocupe todo ponemos False
-        
+
         self.rfid = RfidReader()
-        self.is_reading = True  # Inicialmente, el programa comienza leyendo automáticamente
+        self.is_reading_event = Event()  # Evento para sincronización
+        self.is_reading_event.set()  # Inicialmente, el evento empieza
         self.read_thread = Thread(target=self.read_uid_thread)
         self.read_thread.start()
 
     def clear_uid(self, widget):
-        
         self.label.set_markup('<span size="x-large" weight= "bold" foreground="white">Please, login with your university card</span>')
-        self.label.override_background_color(Gtk.StateFlags.NORMAL, Gdk.RGBA(0, 0, 1, 1))  # Cambia el color de fondo a azul
-        self.is_reading = True #una vez pulsemos el botón, hacemos que vuelva a leer un UID
-        
+        self.label.override_background_color(Gtk.StateFlags.NORMAL, Gdk.RGBA(0.5, 0.5, 1, 1))  # Cambia el color de fondo a azul
+        self.is_reading_event.set()  # Marcar el evento para reanudar la lectura
+
     def read_uid_thread(self):
         while True:
-            if self.is_reading:
-                uid = self.rfid.read_uid()
-                GLib.idle_add(self.show_uid, uid)
-                self.is_reading = False #cambiamos a False para que no siga leyendo y modificando la interfaz.
+            self.is_reading_event.wait()  # Espera a que se permita la lectura
+            uid = self.rfid.read_uid()
+            GLib.idle_add(self.show_uid, uid)
+            self.is_reading_event.clear()  # Reiniciar el evento para detener la lectura
+            # Añadir un mensaje en la consola para mostrar el UID leído
+            print(f"UID leído: {uid}")
 
     def show_uid(self, uid):
         self.label.set_markup('<span size="xx-large" weight= "bold" foreground="white">UID: {0}</span>'.format(uid))
-        self.label.override_background_color(Gtk.StateFlags.NORMAL, Gdk.RGBA(0, 0.85, 0, 1))  # Cambia el color de fondo a verde
-        
+        self.label.override_background_color(Gtk.StateFlags.NORMAL, Gdk.RGBA(0.3, 0.85, 0.3, 1))  # Cambia el color de fondo a verde
+
 if __name__ == "__main__":
     win = RFID_GUI()
     win.connect("destroy", Gtk.main_quit)
     win.show_all()
     Gtk.main()
+
